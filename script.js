2a14be297429e4f870963610c44ea84848bf95f9
Dennis Araujo <dennisaraujo2004@gmail.com>
2025-05-13 19:56:51 -0300

    Att 1.0

--- script.js
+++ script.js
@@ -216,4 +216,13 @@ setInterval(() => {
   typeElement.classList.add('typewriter');
 }, 8000);
 
+
+  // Botao Menu
   
+  const toggle = document.getElementById('menu-toggle');
+  const menu = document.getElementById('menu');
+
+  toggle.addEventListener('click', () => {
+    menu.classList.toggle('show');
+  });
+
